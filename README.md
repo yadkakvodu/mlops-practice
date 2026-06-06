000f34d8d619d8001bf7bf75b52e2a38fd55fbe8
dinislamalipkachev <dalipkachev@inbox.ru>
2026-06-07 01:38:34 +0300

    Fix HF Space deployment

---
+++ README.md
@@ -0,0 +1,8 @@
+---
+title: Iris Api Alipkachev
+emoji: 🌕
+colorFrom: blue
+colorTo: green
+sdk: docker
+pinned: false
+---
--- src/app.py
+++ src/app.py
@@ -4,24 +4,24 @@ from pydantic import BaseModel
 import joblib
 import os
 
-# 1. Определяем абсолютные пути (чтобы избежать ошибок при деплое)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, "..", "models", "model.pkl")
+MODEL_PATH = os.path.join(BASE_DIR, "models", "model.pkl")
 
-# 2. Загружаем модель ОДИН раз при старте сервера! (Best Practice)
 model = joblib.load(MODEL_PATH)
 
 app = FastAPI(title="Iris ML API", description="API для предсказания сорта Ириса")
 
 
-# 3. Редиректим нас сразу на главную страницу.
 @app.get("/")
-def read_root():
-    # Автоматически перенаправляем пользователя на страницу с документацией
+def root():
     return RedirectResponse(url="/docs")
 
 
-# 4. Контракт данных (Pydantic). FastAPI не пропустит запросы с неверными типами.
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
 class IrisFeatures(BaseModel):
     sepal_length: float
     sepal_width: float
@@ -29,16 +29,8 @@ class IrisFeatures(BaseModel):
     petal_width: float
 
 
-# 5. Эндпоинт для проверки здоровья сервера (Uptime Monitor)
-@app.get("/health")
-def health_check():
-    return {"status": "ok", "message": "Server is running"}
-
-
-# 6. Эндпоинт для предсказания (принимает JSON, возвращает JSON)
 @app.post("/predict")
 def predict(features: IrisFeatures):
-    # Преобразуем входящие данные в массив для scikit learn
     data = [
         [
             features.sepal_length,
@@ -47,7 +39,7 @@ def predict(features: IrisFeatures):
             features.petal_width,
         ]
     ]
+
     prediction = model.predict(data)
 
-    # Возвращаем результат
     return {"predicted_class": int(prediction[0])}
